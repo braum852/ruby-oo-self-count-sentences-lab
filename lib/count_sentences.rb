94c8b8d52b1d69146c2aceb22a40e01b7ea02db1
braum852 <julian.teo59@yahoo.com>
2022-02-06 17:23:54 +1000

    Done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with? "."
   end
 
   def question?
-
+    self.end_with? "?"
   end
 
   def exclamation?
-
+    self.end_with? "!"
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).delete_if{|word| word.length < 2}.count
   end
 end
+
+##self.split - splits array when comes across . ? ! 
+##by using regex condition |\ means or
+##.length  returns the number of elements in the array
+##.delete_if means Deletes every element of self for which block evaluates to true.
+##
